d5a8b1367c55e3e3778fd431cf635e14f6a09a4e
Derek Price <derekp098@gmail.com>
2020-12-29 21:24:44 -0800

    added a bunch of bug objects (40ish)

---
+++ assets/javascript/bugsAndFish.js
@@ -0,0 +1,374 @@
+export {bugs};
+//#region bugs
+const bugs = {
+    commonButterfly: {
+    name:"Common Butterfly",
+    picture: './assets/images/bugs/NH-Icon-commonbutterfly.png',
+    price:"160",
+    location:"Flying",
+    time:"4:00 am - 7:00 pm",
+    months: 'Sep - Jun',
+    notes:""
+    },
+    yellowButterfly: {
+    name:"Yellow Butterfly",
+    picture: './assets/images/bugs/NH-Icon-yellowbutterfly.png',
+    price:"160",
+    location:"Flying",
+    time:"4:00 am - 7:00 pm",
+    months: 'Mar - Jun & Sep - Oct',
+    notes:""
+    },
+    tigerButterfly: {
+    name:"Tiger Butterfly",
+    picture: './assets/images/bugs/NH-Icon-tigerbutterfly.png',
+    price:"240",
+    location:"Flying",
+    time:"4:00 am - 7:00 pm",
+    months: 'Mar - Sep',
+    notes:""
+    },
+    peacockButterfly: {
+    name:"Peacock Butterfly",
+    picture: './assets/images/bugs/NH-Icon-peacockbutterfly.png',
+    price:"2500",
+    location:"Flying by Hybrid Flowers",
+    time:"4:00 am - 7:00 pm",
+    months: 'Mar - Jun',
+    notes:"Purple, Black and Blue Flowers."
+    },
+    commonBluebottle: {
+    name:"Commom Bluebottle",
+    picture: './assets/images/bugs/NH-Icon-commonbluebottle.png',
+    price:"300",
+    location:"Flying",
+    time:"4:00 am - 7:00 pm",
+    months: 'Apr - Aug',
+    notes:""
+    },
+    paperKiteButterfly: {
+    name: 'Paper Kite Butterfly',
+    picture: './assets/images/bugs/NH-Icon-paperkitebutterfly.png',
+    price: '1000',
+    location: 'Flying',
+    time: '8:00 am - 7:00 pm',
+    months: 'All Year',
+    notes: ''
+    },
+    greatPurpleEmperor: {
+    name: 'Great Purple Emperor',
+    picture: './assets/images/bugs/NH-Icon-greatpurpleemperor.png',
+    price: '3000',
+    location: 'Flying',
+    time: '4:00 am - 7:00 pm',
+    months: 'May - Aug',
+    notes: ''
+    },
+    monarchButterfly: {
+    name: 'Monarch Butterfly',
+    picture: './assets/images/bugs/NH-Icon-monarchbutterfly.png',
+    price: '140',
+    location: 'Flying',
+    time: '4:00 am - 5:00 pm',
+    months: 'Sep - Nov',
+    notes: ''
+    },
+    emperorButterfly: {
+    name: 'Emperor Butterfly',
+    picture: './assets/images/bugs/NH-Icon-emperorbutterfly.png',
+    price: '4000',
+    location: 'Flying',
+    time: '5:00 pm - 8:00 am',
+    months: 'Dec - Mar & Jun - Sep',
+    notes: ''
+    },
+    agriasButterfly: {
+    name: 'Agrias Butterfly',
+    picture: './assets/images/bugs/NH-Icon-agriasbutterfly.png',
+    price: '3000',
+    location: 'Flying',
+    time: '8:00 am - 5:00 pm',
+    months: 'Apr - Sep',
+    notes: ''
+    },
+    rajahBrookesBirdwing: {
+    name: 'Rajah Brooke\'s Birdwing',
+    picture: './assets/images/bugs/NH-Icon-rajahbrookesbirdwing.png',
+    price: '2500',
+    location: 'Flying',
+    time: '8:00 am - 5:00 pm',
+    months: 'Dec - Feb & Apr - Sep',
+    notes: ''
+    },
+    queenAlexandrasBirdwing: {
+    name: 'Queen Alexandra\'s Birdwing',
+    picture: './assets/images/bugs/NH-Icon-queenalexandrasbirdwing.png',
+    price: '4000',
+    location: 'Flying',
+    time: '8:00 am - 4:00 pm',
+    months: 'May - Sep',
+    notes: ''
+    },
+    moth: {
+    name: 'Moth',
+    picture: './assets/images/bugs/NH-Icon-moth.png',
+    price: '130',
+    location: 'Flying by lights',
+    time: '7:00 pm - 4:00 am',
+    months: 'All Year',
+    notes: ''
+    },
+    atlasMoth: {
+    name: 'Atlas Moth',
+    picture: './assets/images/bugs/NH-Icon-atlasmoth.png',
+    price: '3000',
+    location: 'On trees',
+    time: '7:00 pm - 4:00 am',
+    months: 'Apr - Sep',
+    notes: ''
+    },
+    madagacanSunsetMoth: {
+    name: 'Madagascan Sunset Moth',
+    picture: './assets/images/bugs/NH-Icon-madagascansunsetmoth.png',
+    price: '2500',
+    location: 'Flying',
+    time: '8:00 am - 7:00 pm',
+    months: 'Apr - Sep',
+    notes: ''
+    },
+    longLocust: {
+    name: 'Long Locust',
+    picture: './assets/images/bugs/NH-Icon-longlocust.png',
+    price: '200',
+    location: 'On the ground',
+    time: '8:00 am - 7:00 pm',
+    months: 'Apr - Nov',
+    notes: ''
+    },
+    migratoryLocust: {
+    name: 'Migratory Locust',
+    picture: './assets/images/bugs/NH-Icon-migratorylocust.png',
+    price: '600',
+    location: 'On the ground',
+    time: '8:00 am - 7:00 pm',
+    months: 'Aug - Nov',
+    notes: ''
+    },
+    riceGrasshopper: {
+    name: 'Rice Grasshopper',
+    picture: './assets/images/bugs/NH-Icon-ricegrasshopper.png',
+    price: '160',
+    location: 'On the ground',
+    time: '8:00 am - 7:00 pm',
+    months: 'Aug - Nov',
+    notes: ''
+    },
+    grasshopper: {
+    name: 'Grasshopper',
+    picture: './assets/images/bugs/NH-Icon-grasshopper.png',
+    price: '160',
+    location: 'On the ground',
+    time: '8:00 am - 7:00 pm',
+    months: 'Jul - Sep',
+    notes: ''
+    },
+    cricket: {
+    name: 'Cricket',
+    picture: './assets/images/bugs/NH-Icon-cricket.png',
+    price: '130',
+    location: 'On the ground',
+    time: '5:00 pm - 8:00 am',
+    months: 'Sep - Nov',
+    notes: ''
+    },
+    bellCricket: {
+    name: 'Bell Cricket',
+    picture: './assets/images/bugs/NH-Icon-bellcricket.png',
+    price: '430',
+    location: 'On the ground',
+    time: '5:00 pm - 8:00 am',
+    months: 'Sep - Oct',
+    notes: ''
+    },
+    mantis: {
+    name: 'Mantis',
+    picture: './assets/images/bugs/NH-Icon-mantis.png',
+    price: '430',
+    location: 'On Flowers',
+    time: '8:00 am - 5:00 pm',
+    months: 'Mar - Nov',
+    notes: ''
+    },
+    orchidMantis: {
+    name: 'Orchid Mantis',
+    picture: './assets/images/bugs/NH-Icon-orchidmantis.png',
+    price: '2400',
+    location: 'On Flowers (White)',
+    time: '8:00 am - 5:00 pm',
+    months: 'Mar - Nov',
+    notes: ''
+    },
+    honeyBee: {
+    name: 'Honey Bee',
+    picture: './assets/images/bugs/NH-Icon-honeybee.png',
+    price: '200',
+    location: 'Flying',
+    time: '8:00 am - 5:00 pm',
+    months: 'Mar - Jul',
+    notes: ''
+    },
+    wasp: {
+    name: 'Wasp',
+    picture: './assets/images/bugs/NH-Icon-wasp.png',
+    price: '2500',
+    location: 'Shaking trees',
+    time: 'all day',
+    months: 'All Year',
+    notes: ''
+    },
+    brownCicada: {
+    name: 'Brown Cicada',
+    picture: './assets/images/bugs/NH-Icon-browncicada.png',
+    price: '250',
+    location: 'On trees',
+    time: '8:00 am - 5:00 pm',
+    months: 'Jul - Aug',
+    notes: ''
+    },
+    robustCicada: {
+    name: 'Robust Cicada',
+    picture: './assets/images/bugs/NH-Icon-robustcicada.png',
+    price: '300',
+    location: 'On trees',
+    time: '8:00 am - 5:00 pm',
+    months: 'Jul - Aug',
+    notes: ''
+    },
+    giantCicada: {
+    name: 'Giant Cicada',
+    picture: './assets/images/bugs/NH-Icon-giantcicada.png',
+    price: '500',
+    location: 'On trees',
+    time: '8:00 am - 5:00 pm',
+    months: 'Jul - Aug',
+    notes: ''
+    },
+    walkerCicada: {
+    name: 'Walker Cicada',
+    picture: './assets/images/bugs/NH-Icon-walkercicada.png',
+    price: '400',
+    location: 'On trees',
+    time: '8:00 am - 5:00 pm',
+    months: 'Aug - Sep',
+    notes: ''
+    },
+    eveningCicada: {
+    name: 'Evening Cicada',
+    picture: './assets/images/bugs/NH-Icon-eveningcicada.png',
+    price: '550',
+    location: 'On trees',
+    time: '4:00 am - 8:00 am & 4:00 pm - 7:00 pm',
+    months: 'Jul - Aug',
+    notes: ''
+    },
+    cicadaShell: {
+    name: 'Cicada Shell',
+    picture: './assets/images/bugs/NH-Icon-cicadashell.png',
+    price: '10',
+    location: 'On trees',
+    time: 'all day',
+    months: 'Jul - Aug',
+    notes: ''
+    },
+    redDragonfly: {
+    name: 'Red Dragonfly',
+    picture: './assets/images/bugs/NH-Icon-reddragonfly.png',
+    price: '180',
+    location: 'Flying',
+    time: '8:00 am - 7:00 pm',
+    months: 'Sep - Oct',
+    notes: ''
+    },
+    darnerDragonfly: {
+    name: 'Darner Dragonfly',
+    picture: './assets/images/bugs/NH-Icon-darnerdragonfly.png',
+    price: '230',
+    location: 'Flying',
+    time: '8:00 am - 5:00 pm',
+    months: 'Apr - Oct',
+    notes: ''
+    },
+    bandedDragonfly: {
+    name: 'Banded Dragonfly',
+    picture: './assets/images/bugs/NH-Icon-bandeddragonfly.png',
+    price: '4500',
+    location: 'Flying',
+    time: '8:00 am - 5:00 pm',
+    months: 'May - Oct',
+    notes: ''
+    },
+    damselfly: {
+    name: 'Damselfly',
+    picture: './assets/images/bugs/NH-Icon-damselfly.png',
+    price: '500',
+    location: 'Flying',
+    time: 'all day',
+    months: 'Nov - Feb',
+    notes: ''
+    },
+    firefly: {
+    name: 'Firefly',
+    picture: './assets/images/bugs/NH-Icon-firefly.png',
+    price: '300',
+    location: 'Flying',
+    time: '7:00 pm - 4:00 am',
+    months: 'Jun',
+    notes: ''
+    },
+    moleCricket: {
+    name: 'Mole Cricket',
+    picture: './assets/images/bugs/NH-Icon-molecricket.png',
+    price: '500',
+    location: 'Underground',
+    time: 'all day',
+    months: 'May - Oct',
+    notes: ''
+    },
+    pondskater: {
+    name: 'Pondskater',
+    picture: './assets/images/bugs/NH-Icon-pondskater.png',
+    price: '130',
+    location: 'On Ponds and Rivers',
+    time: '8:00 am - 7:00 pm',
+    months: 'May - Sep',
+    notes: ''
+    },
+    divingBeetle: {
+    name: 'Diving Beetle',
+    picture: './assets/images/bugs/NH-Icon-divingbeetle.png',
+    price: '800',
+    location: 'On Ponds and Rivers',
+    time: '8:00 am - 7:00 pm',
+    months: 'May - Sep',
+    notes: ''
+    },
+    giantWaterBug: {
+    name: 'Giant Water Bug',
+    picture: './assets/images/bugs/NH-Icon-giantwaterbug.png',
+    price: '2000',
+    location: 'On Ponds and Rivers',
+    time: '7:00 pm - 8:00 am',
+    months: 'Apr - Sep',
+    notes: ''
+    },
+    stinkbug: {
+    name: 'Stinkbug',
+    picture: './assets/images/bugs/NH-Icon-stinkbug.png',
+    price: '120',
+    location: 'On Flowers',
+    time: 'all day',
+    months: 'Mar - Oct',
+    notes: ''
+    },
+}
+//#endregion
